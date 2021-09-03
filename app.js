a943bfc6f0e7d9278986eef32645d0f4f967a6de
Alexey <Aleksey.Slipchenko97@gmail.com>
2021-09-03 23:41:46 +0300

    modal

---
+++ app.js
@@ -0,0 +1,24 @@
+let btnSendMessage = document.querySelector(".btn-send")
+let modal = document.querySelector(".modal")
+let btnClose = document.querySelector(".btn-close")
+console.log(btnSendMessage)
+console.log(modal)
+console.log(btnClose)
+
+btnSendMessage.addEventListener("click",openModal);
+
+btnClose.addEventListener("click",closeModal);
+
+function openModal(){
+    modal.classList.remove("hide");
+    modal.classList.add("show");
+    }
+
+    function closeModal(){
+        modal.classList.remove("show");
+        modal.classList.add("hide");
+        }
+    modal.addEventListener("click", function(e){
+        if (e.target === modal) {
+        closeModal()
+        }})
